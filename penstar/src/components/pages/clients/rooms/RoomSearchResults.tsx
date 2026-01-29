518dc06f77a8353ffb46b74f53be42f94e542a11
nguyenduyhung3624 <nguyenduyhung3624@gmail.com>
2026-01-29 17:26:45 +0700

    server fix

--- penstar/src/components/pages/clients/rooms/RoomSearchResults.tsx
+++ penstar/src/components/pages/clients/rooms/RoomSearchResults.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState, useMemo } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { message, Spin, Empty, Button, Row, Col } from "antd";
